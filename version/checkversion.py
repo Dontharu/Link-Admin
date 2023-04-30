67ebfc787aa816660a2f5c0c35c4239867cfa309
Dontharu <98643185+Dontharu@users.noreply.github.com>
2023-04-30 15:16:06 +0530

    Update checkversion.py

--- version/checkversion.py
+++ version/checkversion.py
@@ -26,11 +26,11 @@ else:
 row = []
 info = ''
 result = ''
-version = '1.4.2'
+version = '1.4.3'
 
 def ver_check():
 	print(G + '[+]' + C + ' Checking the Link-Admin for updates....', end='')
-	ver_url = 'https://raw.githubusercontent.com/Dontharu/Link-Admin/version/version.txt'
+	ver_url = 'https://raw.githubusercontent.com/Dontharu/Link-Admin/main/version/version.txt'
 	try:
 		ver_rqst = requests.get(ver_url)
 		ver_sc = ver_rqst.status_code
